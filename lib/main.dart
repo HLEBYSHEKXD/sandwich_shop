d131078a23d777da43654195f1a906fd7e3dc4a8
Your Name <mishail.nevrov@gmail.com>
2025-10-12 20:25:13 +0100

    Change tab title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Sandwich Shop App',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color.fromARGB(255, 153, 105, 199),
+          seedColor: const Color.fromARGB(255, 148, 48, 241),
         ),
       ),
       home: const MyHomePage(title: 'My Sandwich Shop'),
