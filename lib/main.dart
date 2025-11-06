6367980ddc8c9079a7ab769c6c1ea8b69ffcdbf6
Your Name <mishail.nevrov@gmail.com>
2025-11-06 19:49:01 +0000

    Dissable buttons on limit

--- lib/main.dart
+++ lib/main.dart
@@ -64,7 +64,8 @@ class _OrderScreenState extends State<OrderScreen> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 ElevatedButton(
-                  onPressed: _increaseQuantity,
+                  onPressed:
+                      _quantity < widget.maxQuantity ? _increaseQuantity : null,
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.green,
                     foregroundColor: Colors.white,
@@ -72,7 +73,7 @@ class _OrderScreenState extends State<OrderScreen> {
                   child: const Text('Add'),
                 ),
                 ElevatedButton(
-                  onPressed: _decreaseQuantity,
+                  onPressed: _quantity > 0 ? _decreaseQuantity : null,
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.red,
                     foregroundColor: Colors.white,
