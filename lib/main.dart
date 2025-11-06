e206bf657f19c74c8276e4576bfcfdf4901fdb38
Your Name <mishail.nevrov@gmail.com>
2025-11-06 18:39:24 +0000

    Change the displayable text

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class App extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(title: const Text('Sandwich Counter')),
         body: const Center(
-          child: OrderItemDisplay(5, 'Footlong'),
+          child: Text('Welcome to the Sandwich Shop!'),
         ),
       ),
     );
